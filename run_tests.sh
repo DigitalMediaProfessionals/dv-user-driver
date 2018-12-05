f153442d12ec520176029559a763cb5327a5fce0
Alexey Kazantsev <alexey.kazantsev@dmprof.com>
2018-12-05 15:24:07 +0900

    Removed run_tests.sh

--- run_tests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo env LD_LIBRARY_PATH=`pwd` sh -c 'cd tests/test_context && ./test_context && cd ../test_mem && ./test_mem 64 && cd ../test_weights && ./test_weights && cd ../test_conv && ./test_conv && cd ../test_fc && ./test_fc'
